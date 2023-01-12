fc448e4116dcbe02f59106e2042f8a912d3f1e63
Son Pham-Ba <son.phamba@epfl.ch>
2023-01-12 14:07:42 +0100

    style(struct-out): tabs to spaces

--- analysis/structuredOutput.py
+++ analysis/structuredOutput.py
@@ -18,32 +18,30 @@ draw = ImageDraw.Draw(im, "RGBA")
 
 # Define text style
 fontPath = "ressources/Roboto-Regular.ttf"
-textColor = (0, 0, 0) # black
+textColor = (0, 0, 0)  # black
 
 # Text area style
-fill = (255, 255, 255, 192) # semi-transparent white
+fill = (255, 255, 255, 192)  # semi-transparent white
 lineWidth = 3
-outline = (192, 0, 0) # dark red
+outline = (192, 0, 0)  # dark red
 
 # convertion helper function
 def pointFromData(data):
-	return (
-		data["x"] * im.width,
-		(1 - data["y"]) * im.height # inverted vertical axis
-	)
+    return (data["x"] * im.width, (1 - data["y"]) * im.height)  # inverted vertical axis
+
 
 for detectedText in data:
-	text = detectedText["text"]
-	points = [pointFromData(detectedText["bbox"][name]) for name in POINT_NAMES]
-	textPos = points[3] # topLeft
+    text = detectedText["text"]
+    points = [pointFromData(detectedText["bbox"][name]) for name in POINT_NAMES]
+    textPos = points[3]  # topLeft
 
-	fontSize = round(points[0][1] - points[3][1]) # height if bbox, in pixels
-	if fontSize < 0: fontSize = 0
-	font = ImageFont.truetype(fontPath, fontSize) # TODO: change arbitrary font size
+    fontSize = round(points[0][1] - points[3][1])  # height if bbox, in pixels
+    if fontSize < 0:
+        fontSize = 0
+    font = ImageFont.truetype(fontPath, fontSize)  # TODO: change arbitrary font size
 
-	draw.polygon(points, outline=outline, width=lineWidth, fill=fill)
-	draw.text(textPos, text, fill=textColor, font=font)
-	print(text)
-	
+    draw.polygon(points, outline=outline, width=lineWidth, fill=fill)
+    draw.text(textPos, text, fill=textColor, font=font)
+    print(text)
+
 im.show()
-
