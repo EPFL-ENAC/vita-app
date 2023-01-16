0290f75d2758aa8e9d4b082420378410b8216183
Son Pham-Ba <son.phamba@epfl.ch>
2023-01-16 17:43:29 +0100

    feat(struct-out): test usage of string searching functions

--- analysis/readers/alconEx500.py
+++ analysis/readers/alconEx500.py
@@ -1,7 +1,35 @@
+import regex
+from readers import search
+from BoundingBox import BoundingBox
+
+
 distinctivePattern = "EX500.+Treatment Report"
+
+datePattern = r"\d{2}?\.\d{2}?\.\d{4}?"
+timePattern = r"\d{2}?:\d{2}?:\d{2}?"
+dateTimePattern = f"{datePattern} {timePattern}"
 
 
 def read(detectedTextList):
-    data = {}
+    data = []
+
+    # Name
+    # Birth date
+    # Eye (OS/OD)
+    c = search.string(detectedTextList, "O[SD]", BoundingBox.fromBounds(0.2, 0.7, 0.7, 1))
+    if len(c) != 0:
+        data.append({ "eye": c[0].regexMatch.captures()[0] })
+
+    # Gender
+    # Treatment date
+    c = search.string(detectedTextList, "Treatment date", BoundingBox.fromBounds(0.5, 1, 0.7, 1))
+    if len(c) != 0:
+        c = search.stringOnRight(c[0], detectedTextList, dateTimePattern, 10)
+        if c is not None:
+            data.append({ "Treatment date": c.regexMatch.captures()[0] })
+
+
+    print(data)
+
     filteredDetectedText = detectedTextList
     return data, filteredDetectedText
