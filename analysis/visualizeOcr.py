d6361170e9af6caf0299815dcb56914be9da9f71
Son Pham-Ba <son.phamba@epfl.ch>
2023-01-26 15:04:32 +0100

    fead(struct-out): saving images with all and filtered ocr

--- analysis/visualizeOcr.py
+++ analysis/visualizeOcr.py
@@ -4,7 +4,7 @@ import image
 
 
 if len(sys.argv) != 2:
-    print("\nUsage: python3 structuredOutput.py path_to_json")
+    print("\nUsage: python3 visualizeOcr.py path_to_json")
     quit()
 
 filename = sys.argv[1]
@@ -13,11 +13,14 @@ if jsonIndex == -1:
     print("\nError: specified file is not a json")
     quit()
 
+# Remove .json extension
 filename = filename[:jsonIndex]
+outname = filename.replace("inputs/", "outputs/")
 
 
 # import from iOS output
 allDetectedText = DetectedText.fromFile(f"{filename}.json")
 
 # Show image
-image.generate(f"{filename}.png", allDetectedText)
+im = image.generate(f"{filename}.png", allDetectedText)
+image.save(im, f"{outname}_all.png")
