2f969132a0c4c26b14cafae92c6bdf327c13cdf9
Son Pham-Ba <son.phamba@epfl.ch>
2023-01-26 14:49:20 +0100

    feat(struct-out): support output directory creation matching input path

--- analysis/csvWriter.py
+++ analysis/csvWriter.py
@@ -1,7 +1,11 @@
 import csv
+import os
 
 
 def write(data, path):
+    # Create the directory if it doesn't exist
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
     f = open(path, "w")
     writer = csv.writer(f)
     writer.writerow([list(entry.keys())[0] for entry in data])
