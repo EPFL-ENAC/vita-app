7b939beb5f4f2c7ddd3c45c28895baecbe51d6c3
Son Pham-Ba <son.phamba@epfl.ch>
2023-01-27 17:46:26 +0100

    feat(struct-out): added processing of multiple files at once

--- analysis/visualizeOcr.py
+++ analysis/visualizeOcr.py
@@ -1,26 +1,32 @@
 import sys
+from utils import getListOfJsonFilenames
 import DetectedText
 import image
 
 
 if len(sys.argv) != 2:
-    print("\nUsage: python3 visualizeOcr.py path_to_json")
+    print("\nUsage: python3 structuredOutput.py path_to_json")
+    print("If path_to_json is a directory, all the json files in the directory tree will be processed.")
     quit()
 
-filename = sys.argv[1]
-jsonIndex = filename.find(".json")
-if jsonIndex == -1:
-    print("\nError: specified file is not a json")
-    quit()
-
-# Remove .json extension
-filename = filename[:jsonIndex]
-outname = filename.replace("inputs/", "outputs/")
+filenames = getListOfJsonFilenames(sys.argv[1])
 
 
-# import from iOS output
-allDetectedText = DetectedText.fromFile(f"{filename}.json")
+# Loop through all the json files
+for filename in filenames:
+    print(f"\nProcessing {filename}\n")
 
-# Show image
-im = image.generate(f"{filename}.png", allDetectedText)
-image.save(im, f"{outname}_all.png")
+    # Remove .json extension
+    filename = filename[:filename.rfind(".json")]
+    outname = filename.replace("inputs/", "outputs/")
+
+    # import from iOS output
+    allDetectedText = DetectedText.fromFile(f"{filename}.json")
+
+    # Generate image with detected text
+    im = image.generate(f"{filename}.png", allDetectedText)
+    image.save(im, f"{outname}_filtered.png")
+
+    # Show image only if one file is processed
+    if len(filenames) == 1:
+        image.show(im)
