6508e3f08c7ddd3f71f6e4abdd2527873cb2e7c9
Son Pham-Ba <son.phamba@epfl.ch>
2023-01-31 10:50:54 +0100

    feat(compare): added comparison scripts

---
+++ analysis/compare.py
@@ -0,0 +1,73 @@
+import sys
+import pandas as pd
+import Levenshtein
+
+
+if len(sys.argv) != 3:
+    print("\nUsage: python3 compare.py reference file")
+    print("'reference' and 'file' can be .csv or .xlsx files")
+    quit()
+
+referencePath = sys.argv[1]
+filePath = sys.argv[2]
+
+
+dateFormat = "%d/%m/%Y"
+
+
+# Load tables
+def load(path):
+    if path.endswith(".csv"):
+        return pd.DataFrame(pd.read_csv(path))
+    elif path.endswith(".xlsx"):
+        return pd.read_excel(path)
+    else:
+        print("File format not supported")
+        quit()
+
+reference = load(referencePath)
+file = load(filePath)
+totalDist = 0
+
+# Compare each entry
+for col in reference:
+    if col not in file:
+        print(f"Column '{col}' not found in file")
+
+    target = reference[col][0]
+    read = file[col][0]
+
+
+    # Reformat last name (full caps in documents)
+    if col == "Last name":
+        target = target.title()
+        read = read.title()
+
+    # Format dates
+    if type(target) == pd.Timestamp:
+        target = target.strftime(dateFormat)
+    if type(read) == pd.Timestamp:
+        read = read.strftime(dateFormat)
+
+
+    # Maching
+    if target == read or (pd.isna(target) and pd.isna(read)):
+        if pd.isna(target):
+            print(f"{col}:")
+        else:
+            print(f"{col}: {target}")
+
+    # Not matching
+    else:
+        target = str(target)
+        read = str(read)
+        if target == "nan":
+            target = ""
+        if read == "nan":
+            read = ""
+        dist = Levenshtein.distance(target, read)
+        totalDist += dist
+        print(f"{col}: {read} (expected '{target}', distance {dist})")
+
+
+print(f"\nTotal distance: {totalDist}\n")
