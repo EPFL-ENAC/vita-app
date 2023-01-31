afe356244f59f42ff0a0db23a0279640343a5355
Son Pham-Ba <son.phamba@epfl.ch>
2023-01-31 17:44:08 +0100

    feat(compare): also convert 0,00 float represented with comma

--- analysis/compareAllSophtalmo.py
+++ analysis/compareAllSophtalmo.py
@@ -2,6 +2,8 @@ from os import system
 
 for i in range(1, 11):
     for app in ["", ".2"]:
+        if i == 10 and app == "":
+            continue
         filename = f"patient{i}{app}"
         cmd = f"python3 compare.py references/sophtalmo-screenshots/{filename}.xlsx outputs/sophtalmo-screenshots/{filename}.csv"
         print(cmd)
